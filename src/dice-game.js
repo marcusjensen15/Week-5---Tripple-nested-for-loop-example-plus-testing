5aa5c0ebd40fd84265205d49135a01950f52541f
Student <example@example.com>
2019-11-14 16:37:09 -0800

    add code to count vowels and runs practice tests

--- src/dice-game.js
+++ src/dice-game.js
@@ -1,33 +1,84 @@
-// export function test() {
-//   console.log('this is the back-end');
-// } test();
-
 //Example code for running tests in jest.test.js//
 
-export var MyObject = function(number1,number2,number3){
-
-  this.number1 = number1,
-  this.number2= number2,
-  this.number3 = number3
-
-}
-
-MyObject.prototype.multiplyNumbers = function() {
-  var multiply = this.number1 * this.number2;
-  return multiply;
-}
-
-export var MyObject2 = function(number1,number2,number3){
-
-  this.number1 = number1,
-  this.number2= number2,
-  this.number3 = number3
-
-}
-
-MyObject2.prototype.multiplyNumbers = function() {
-  var multiply = this.number1 + this.number2;
-  return multiply;
-}
+// export var MyObject = function(number1,number2,number3){
+//
+//   this.number1 = number1,
+//   this.number2= number2,
+//   this.number3 = number3
+//
+// }
+//
+// MyObject.prototype.multiplyNumbers = function() {
+//   var multiply = this.number1 * this.number2;
+//   return multiply;
+// }
+//
+// export var MyObject2 = function(number1,number2,number3){
+//
+//   this.number1 = number1,
+//   this.number2= number2,
+//   this.number3 = number3
+//
+// }
+//
+// MyObject2.prototype.multiplyNumbers = function() {
+//   var multiply = this.number1 + this.number2;
+//   return multiply;
+// }
 
 //Example code for running tests in jest.test.js//
+//
+// FrontEnd:
+//
+// -3 Inputs
+//   First line has five syllables
+//   Second line has seven syllables
+//   Third has five syllables
+//
+// -Area to confirm if the poem or not is a Haiku
+//
+// Backend:
+// -check if poem has 3 lines
+// -need to check to make sure the 1st and 3rd lines have 5 syllables
+//
+//
+// -function to count number of syllables in a string
+//   -Each syllable is represented by a vowel until it hits a consanant (or multiple vowels)
+//   -if the word ends with a vowel it is ignored that vowel is ignored
+//   -if more than one vowel is in a row, the following vowels are ignored
+
+// console.log("hello");
+//
+//   export var test4 = function() {
+//   console.log('this is the back-end 2');
+// };
+// export var test5 = function() {
+// console.log('this is the back-end 26');
+// };
+//
+// test4();
+// test5();
+
+export var MyLetters = function(){
+   this.vowels = ["a","e","i","o","u"];
+   this.consanants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"];
+}
+
+export var Poem = function(lineOne, lineTwo, lineThree){
+  this.lineOne = lineOne
+  this.lineTwo = lineTwo
+  this.lineThree = lineThree
+  this.syllableCount = 0;
+}
+
+Poem.prototype.convertString = function(string){
+  var result = string.split(" ");
+  return result;
+}
+
+Poem.prototype.syllables = function(){
+
+}
+
+// export var vowels = ["a","e","i","o","u"];
+// export var consanants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"];
--- src/main.js
+++ src/main.js
@@ -1,9 +1,44 @@
-// import { test } from  './dice-game.js';
+import { MyLetters, Poem} from  './dice-game.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-// function test2() {
-//   console.log('this is the front end');
-// } test2();
+var myLetters = new MyLetters();
+
+console.log(myLetters.vowels);
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    const lineOne = $("input#poemInput1").val();
+    const lineTwo = $("input#poemInput2").val();
+    const lineThree = $("input#poemInput3").val();
+    let poem = new Poem(lineOne, lineTwo, lineThree)
+
+    var newLineOne = poem.convertString(lineOne);
+    var newLineTwo = poem.convertString(lineTwo);
+    var newLineThree = poem.convertString(lineThree);
+
+    var a = newLineOne[0];
+
+    function counter(){
+
+      var wordCount = 0;
+      for (var b = 0; b < newLineOne.length; b++){
+          for (var i=0; i < newLineOne[b].length; i++){
+              for (var j =0; j < myLetters.vowels.length; j++){
+                if (newLineOne[b][i] == myLetters.vowels[j]){
+                  wordCount += 1;
+
+                }
+              }
+
+          }
+        }
+      return wordCount
+    }
+      console.log(counter(a));
+  });
+
+});
